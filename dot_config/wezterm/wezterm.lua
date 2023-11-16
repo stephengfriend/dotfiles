9d8c412a42355f62c7cb59475822daafd5b67beb
Stephen G. Friend <sfriend@blackpointcyber.com>
2023-11-16 10:45:04 -0500

    Tmux, fixes and more

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,8 +8,15 @@ end
 
 config.color_scheme = 'Dracula'
 config.enable_tab_bar = false
-config.font = wezterm.font 'DankMono Nerd Font'
+config.font = wezterm.font "DankMono Nerd Font"
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.window_padding = {
+  left = 16,
+  right = 16,
+  top = 48,
+  bottom = 16,
+}
 
 return config
