a1d656ce71255eb1a0e78fac94481590b70866ed
Stephen G. Friend <stephen.friend@onebrief.com>
2025-02-13 21:09:56 -0500

    update font and word skip with arrows

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ end
 
 config.color_scheme = 'Dracula'
 config.enable_tab_bar = false
-config.font = wezterm.font "DankMono Nerd Font"
+config.font = wezterm.font "MonaspiceAr Nerd Font Mono"
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
