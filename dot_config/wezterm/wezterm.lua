eddb9d0f025cf79e53eacf714e2708ecfb7c1f9f
Stephen G. Friend <stephen.friend@onebrief.com>
2025-05-30 22:14:40 -0400

    backup changes

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -11,6 +11,19 @@ config.enable_tab_bar = false
 config.font = wezterm.font "MonaspiceAr Nerd Font Mono"
 config.font_size = 14
 config.hide_tab_bar_if_only_one_tab = true
+config.keys = {
+  -- Rebind OPT-Left, OPT-Right as ALT-b, ALT-f respectively to match Terminal.app behavior
+  {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendKey { key = 'b', mods = 'ALT' },
+  },
+  {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = wezterm.action.SendKey { key = 'f', mods = 'ALT' },
+  },
+}
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.window_padding = {
   left = 16,
